142d839c632f1750eb17ac55cb17ef411be98e66
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-09-20 21:30:18 +0300

    Vector,text split

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import bs4
 from dotenv import load_dotenv
 from langchain import hub
+from langchain.schema import vectorstore
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.output_parsers import StrOutputParser
@@ -15,6 +16,15 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
 
 loader = WebBaseLoader(web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",))
 docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001"))
+
+retriever = vectorstore.as_retriever()
+
+def format_docs(docs: list):
+    return "\n".join(doc.page_content for doc in docs)
 
 if __name__ == '__main__':
     print(docs)
