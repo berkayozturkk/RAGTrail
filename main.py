4edcb495bb56beb21fbbb97a2bdfb8d1bd7a05bd
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-09-20 21:20:27 +0300

    WebBaseLoader load

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import bs4
 from dotenv import load_dotenv
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-if __name__ == '__main__':
-    print("RAG")
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
 
+loader = WebBaseLoader(web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",))
+docs = loader.load()
+
+if __name__ == '__main__':
+    print(docs)
